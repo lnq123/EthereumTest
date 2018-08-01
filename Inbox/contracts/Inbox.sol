f94deb47659c6130162a74cbb9a252a68321622b
lnq123 <nanqingl@uci.edu>
2018-07-31 19:26:14 -0700

    deploy
    53

--- Inbox/contracts/Inbox.sol
+++ Inbox/contracts/Inbox.sol
@@ -3,12 +3,11 @@ pragma solidity ^0.4.17;
 contract Inbox {
     string public message;
 
-    function Inbox(string initialMessage) public {
+    function inbox(string initialMessage) public {
         message = initialMessage;
     }
-
+    
     function setMessage(string newMessage) public {
         message = newMessage;
     }
-
 }
